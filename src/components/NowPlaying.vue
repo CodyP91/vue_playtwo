7043d274626297c4e9e034f2aa4416b810b94c92
Cody <cjoel91@hotmail.com>
2023-05-13 05:47:28 -0600

    Updates

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -1,25 +1,19 @@
 <template>
-    <div>
-      <h2>Now Playing</h2>
-      <div v-if="song">
-        <h3>{{ song.title }} - {{ song.artist }}</h3>
-        <img :src="song.imageurl" :alt="song.title" width="200">
-      </div>
-      <div v-else>
-        <p>Please select a song to play.</p>
-      </div>
+  <div>
+    <h2>Now Playing</h2>
+    <div v-if="currentSong">
+      <img :src="currentSong.imageurl" alt="Song artwork" />
+      <p>{{ currentSong.title }} - {{ currentSong.artist }}</p>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      song: {
-        type: Object,
-        required: false,
-        default: null
-      }
-    }
-  };
-  </script>
-  
+    <p v-else>No song is playing...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NowPlaying',
+  props: {
+    currentSong: Object,
+  },
+};
+</script>
